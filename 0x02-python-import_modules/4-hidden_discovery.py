c30e043d23307c38edfbf5ae5c429b9e7a0587e1
sumaya256 <97213223+sumaya256@users.noreply.github.com>
2022-09-24 15:21:02 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,7 +5,7 @@ import hidden_4 as h
 def discover():
     name = dir(h)
     for i in name:
-    if i[:2] != '__':
-    print("{:s}".format(i))
+         if i[:2] != '__':
+              print("{:s}".format(i))
 if __name__ == "__main__":
     discover()
