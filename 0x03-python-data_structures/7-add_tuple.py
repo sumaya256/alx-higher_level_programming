cdfaa79e4e263b5f9f51f42b5d1c2aa2e3237c58
sumaya256 <sssumaya1@gmail.com>
2022-09-25 16:59:02 +0000

    here

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    if len(tuple_a) < 2:
+
+        if len(tuple_a) < 1:
+
+            tuple_a = tuple_a + (0, 0,)
+
+        else:
+
+            tuple_a = tuple_a + (0,)
+
+    if len(tuple_b) < 2:
+
+        if len(tuple_b) < 2:
+
+            if len(tuple_b) < 1:
+
+                tuple_b = tuple_b + (0, 0,)
+
+            else:
+
+                tuple_b = tuple_b + (0,)
+
+
+
+    x, y = tuple_a
+
+    i, j = tuple_b
+
+
+
+    new_tuple = ()
+
+
+
+    for u in range(len(tuple_a)):
+
+        new_tuple = x + i, y + j
+
+    return 
