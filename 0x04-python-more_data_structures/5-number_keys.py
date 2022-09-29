3488689d7423c92605914924ac23d85870414f46
sumaya256 <sssumaya1@gmail.com>
2022-09-29 16:00:38 +0000

    task2

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+
+def number_keys(a_dict):
+
+    """
+
+    return the number of keys in a dict
+
+    """
+
+    if a_dict is None:
+
+        return None
+
+    return len(a_dict.keys())
